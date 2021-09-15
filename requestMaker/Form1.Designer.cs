8dcb4071da916f78f08ae5cb8d940b62f45deb2c
erhan <erhan11040@gmail.com>
2021-09-15 16:15:00 +0300

    initialize project

---
+++ requestMaker/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace requestMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lwSuccess = new System.Windows.Forms.ListView();
+            this.lwFail = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbRequest = new System.Windows.Forms.Label();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbAvarage = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbSuccess = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lbFailed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(444, 73);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(545, 73);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lwSuccess
+            // 
+            this.lwSuccess.HideSelection = false;
+            this.lwSuccess.Location = new System.Drawing.Point(24, 45);
+            this.lwSuccess.Name = "lwSuccess";
+            this.lwSuccess.Size = new System.Drawing.Size(99, 349);
+            this.lwSuccess.TabIndex = 2;
+            this.lwSuccess.UseCompatibleStateImageBehavior = false;
+            // 
+            // lwFail
+            // 
+            this.lwFail.HideSelection = false;
+            this.lwFail.Location = new System.Drawing.Point(161, 45);
+            this.lwFail.Name = "lwFail";
+            this.lwFail.Size = new System.Drawing.Size(98, 349);
+            this.lwFail.TabIndex = 3;
+            this.lwFail.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "successful requests";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "failed requests";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(288, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Time Elapsed";
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Location = new System.Drawing.Point(288, 73);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(71, 13);
+            this.lbTime.TabIndex = 7;
+            this.lbTime.Text = "Time Elapsed";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(288, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Request Count";
+            // 
+            // lbRequest
+            // 
+            this.lbRequest.AutoSize = true;
+            this.lbRequest.Location = new System.Drawing.Point(288, 136);
+            this.lbRequest.Name = "lbRequest";
+            this.lbRequest.Size = new System.Drawing.Size(78, 13);
+            this.lbRequest.TabIndex = 9;
+            this.lbRequest.Text = "Request Count";
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Location = new System.Drawing.Point(444, 47);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(176, 20);
+            this.tbUrl.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(406, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Url:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(288, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Avarage milisec";
+            // 
+            // lbAvarage
+            // 
+            this.lbAvarage.AutoSize = true;
+            this.lbAvarage.Location = new System.Drawing.Point(288, 199);
+            this.lbAvarage.Name = "lbAvarage";
+            this.lbAvarage.Size = new System.Drawing.Size(81, 13);
+            this.lbAvarage.TabIndex = 14;
+            this.lbAvarage.Text = "Avarage milisec";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(288, 239);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Successfull:";
+            // 
+            // lbSuccess
+            // 
+            this.lbSuccess.AutoSize = true;
+            this.lbSuccess.Location = new System.Drawing.Point(288, 271);
+            this.lbSuccess.Name = "lbSuccess";
+            this.lbSuccess.Size = new System.Drawing.Size(23, 13);
+            this.lbSuccess.TabIndex = 16;
+            this.lbSuccess.Text = "scs";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(288, 310);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Failed:";
+            // 
+            // lbFailed
+            // 
+            this.lbFailed.AutoSize = true;
+            this.lbFailed.Location = new System.Drawing.Point(288, 344);
+            this.lbFailed.Name = "lbFailed";
+            this.lbFailed.Size = new System.Drawing.Size(10, 13);
+            this.lbFailed.TabIndex = 18;
+            this.lbFailed.Text = "f";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbFailed);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lbSuccess);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lbAvarage);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbUrl);
+            this.Controls.Add(this.lbRequest);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lwFail);
+            this.Controls.Add(this.lwSuccess);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListView lwSuccess;
+        private System.Windows.Forms.ListView lwFail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbRequest;
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbAvarage;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbSuccess;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lbFailed;
+    }
+}
+
---
+++ requestMaker/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace requestMaker
+{
+    public partial class Form1 : Form
+    {
+        private int timeElapsed = 0;
+        private int successfullRequestCount=0;
+        private int failedRequestCount=0;
+        private List<long> allRequests;
+        private bool isWorking = false;
+        private Stopwatch watchTotal;
+        private Stopwatch watchRequest;
+
+        public Form1()
+        {
+            InitializeComponent();
+            watchRequest = new Stopwatch();
+            watchTotal = new Stopwatch();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            isWorking = false;
+            watchTotal.Stop();
+        }
+        private void reset()
+        {
+            timeElapsed = 0;
+            successfullRequestCount = 0;
+            failedRequestCount = 0;
+            allRequests = new List<long>();
+            lwSuccess.Clear();
+            lwFail.Clear();
+            watchRequest.Stop();
+            watchTotal.Stop();
+            watchRequest.Reset();
+            watchTotal.Reset();
+        }
+        private void updateLabels()
+        {
+            double average = allRequests.Count > 0 ? allRequests.Average() : 0.0;
+            lbAvarage.Text = average.ToString("0.##");
+            lbFailed.Text = failedRequestCount.ToString();
+            lbSuccess.Text = successfullRequestCount.ToString();
+            lbRequest.Text = allRequests.Count.ToString();
+            watchRequest.Reset();
+        }
+        private string calculateElapsed(Stopwatch watch)
+        {
+            var ts = watch.Elapsed;
+            string elapsedTime = $"{ts.Hours}:{ts.Minutes}:{ts.Seconds}.{ts.Milliseconds / 10}";
+            return elapsedTime;
+        }
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            reset();
+            isWorking = true;
+            watchTotal.Start();
+            while (isWorking)
+            {
+                long elapsed = 0;
+                HttpWebResponse response;
+                lbTime.Text = calculateElapsed(watchTotal);
+                watchRequest.Start();
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(tbUrl.Text);
+                    response = (HttpWebResponse)await request.GetResponseAsync();
+                    string content = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                    watchRequest.Stop();
+                    elapsed = watchRequest.ElapsedMilliseconds;
+                }
+                catch (Exception ex)
+                {
+                    elapsed = watchRequest.ElapsedMilliseconds;
+                    lwFail.Items.Add(new ListViewItem(elapsed.ToString()));
+                    failedRequestCount++;
+                    continue;
+                }
+               
+                watchRequest.Stop();
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    lwSuccess.Items.Add(new ListViewItem(elapsed.ToString()));
+                    successfullRequestCount++;
+                }
+                else
+                {
+                    lwFail.Items.Add(new ListViewItem(elapsed.ToString()));
+                    failedRequestCount++;
+                }
+                allRequests.Add(elapsed);
+                updateLabels();
+            }
+        }
+
+    }
+}
